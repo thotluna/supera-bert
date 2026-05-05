f9c310f51128044f3655f7ad2e1e7f891ed7ffbb
Eladio Feijoo <eladio.feijoo@gmail.com>
2026-05-05 16:28:53 +0200

    chore: upgrade storybook to v10 and fix global config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,12 @@ const config: StorybookConfig = {
   "stories": [
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": ["@storybook/addon-a11y", "essentials"],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@storybook/addon-interactions",
+    "@storybook/addon-links"
+  ],
   "framework": "@storybook/nextjs-vite",
   "staticDirs": [
     "../public"
